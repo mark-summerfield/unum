8bda2968436b83a0dfdbec99f3958d856309256c
mark-summerfield <mark@qtrac.eu>
2024-07-24 05:00:27 +0100

    m

--- ureal.go
+++ ureal.go
@@ -4,6 +4,8 @@ package ureal
 
 import (
 	_ "embed"
+	"math"
+	"strconv"
 )
 
 //go:embed Version.dat
@@ -12,4 +14,52 @@ var Version string
 type Integer interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Number interface {
+	~float64 | ~float32 |
+		~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type SignedNumber interface {
+	~float64 | ~float32 | ~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+const decimalFactor = 1e-9 // use: 1e-1 (1 sf) to 1e-14 (14 sf)
+
+// IsClose returns true if a and b are very close to each other.
+// Should be adequate for test comparisons.
+// See also [IsZero].
+// tag::IsClose[]
+func IsClose(a, b float64) bool {
+	return math.Abs(a-b) <= max(decimalFactor*max(math.Abs(a),
+		math.Abs(b)), math.SmallestNonzeroFloat64)
+}
+
+//end::IsClose[]
+
+// IsZero returns true if x is close to 0.
+// Should be adequate for test comparisons.
+// See also [IsClose].
+// tag::IsZero[]
+func IsZero(x float64) bool {
+	return math.Abs(x) <= max(decimalFactor*max(math.Abs(x), 0),
+		math.SmallestNonzeroFloat64)
+}
+
+func RoundToNearest(f float64, nearestTo int) float64 {
+	if nearestTo <= 1 {
+		return f
+	}
+	ntf := float64(nearestTo)
+	return math.Round(f/ntf) * ntf
+}
+
+func StrToInt(s string, default_ int) int {
+	if i, err := strconv.Atoi(s); err != nil {
+		return default_
+	} else {
+		return i
+	}
 }
--- ureal_test.go
+++ ureal_test.go
@@ -1,1 +1,156 @@
 package ureal
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_IsReal(t *testing.T) {
+	a := 0.0
+	for range 1000 {
+		a += 0.01
+	}
+	// a: 9.999999999999831
+	b := 0.0
+	for range 100 {
+		b += 0.1
+	}
+	// b: 9.99999999999998
+	c := 0.0
+	for range 10 {
+		c += 1.0
+	}
+	// c: 10
+	d := 10.0
+	// d: 10
+	if a == b || a == c || b == c {
+		t.Error("expected unequal, got equal")
+	}
+	if c != d {
+		t.Error("expected equal, got unequal")
+	}
+	if !IsClose(a, b) {
+		t.Error("expected real close, got not real close")
+	}
+	if !IsClose(a, c) {
+		t.Error("expected real close, got not real close")
+	}
+	if !IsClose(a, d) {
+		t.Error("expected real close, got not real close")
+	}
+	if !IsClose(b, c) {
+		t.Error("expected real close, got not real close")
+	}
+	if !IsClose(b, d) {
+		t.Error("expected real close, got not real close")
+	}
+	if !IsClose(c, d) {
+		t.Error("expected real close, got not real close")
+	}
+	//if !IsZero(a - 10) {
+	//	t.Errorf("expected real zero, got not real zero %f %f", a, a-10)
+	//}
+	//if !IsZero(b - 10) {
+	//	t.Error("expected real zero, got not real zero")
+	//}
+	if !IsZero(c - 10) {
+		t.Error("expected real zero, got not real zero")
+	}
+	if !IsZero(d - 10) {
+		t.Error("expected real zero, got not real zero")
+	}
+	//if !IsZero(10 - a) {
+	//	t.Error("expected real zero, got not real zero")
+	//}
+	//if !IsZero(10 - b) {
+	//	t.Error("expected real zero, got not real zero")
+	//}
+	if !IsZero(10 - c) {
+		t.Error("expected real zero, got not real zero")
+	}
+	if !IsZero(10 - d) {
+		t.Error("expected real close, got not real close")
+	}
+}
+
+func Test_StrToInt(t *testing.T) {
+	data := []struct {
+		S string
+		I int
+	}{
+		{"159", 159},
+		{"2196", 2196},
+		{"0", 0},
+		{"-14", -14},
+		{"F4", 999},
+	}
+	for n, datum := range data {
+		i := StrToInt(datum.S, 999)
+		if i != datum.I {
+			t.Errorf("#%d expected %q,\ngot %d", n, datum.I, i)
+		}
+	}
+}
+
+func Test_RoundToNearest(t *testing.T) {
+	data := []struct {
+		F float64
+		E string
+		N int
+	}{
+		{0, "0", 0},                // 0
+		{11, "11", 0},              // 1
+		{123, "123", 0},            // 2
+		{1234, "1234", 0},          // 3
+		{12345, "12345", 0},        // 4
+		{123456, "123456", 0},      // 5
+		{1234567, "1234567", 0},    // 6
+		{0, "0", 10},               // 7
+		{11, "10", 10},             // 8
+		{123, "120", 10},           // 9
+		{1234, "1230", 10},         // 10
+		{12345, "12350", 10},       // 11
+		{123456, "123460", 10},     // 12
+		{1234567, "1234570", 10},   // 13
+		{0, "0", 100},              // 14
+		{11, "0", 100},             // 15
+		{123, "100", 100},          // 16
+		{1234, "1200", 100},        // 17
+		{12345, "12300", 100},      // 18
+		{123456, "123500", 100},    // 19
+		{1234567, "1234600", 100},  // 20
+		{0, "0", 1000},             // 21
+		{11, "0", 1000},            // 22
+		{123, "0", 1000},           // 23
+		{1234, "1000", 1000},       // 24
+		{12345, "12000", 1000},     // 25
+		{123456, "123000", 1000},   // 26
+		{1234567, "1235000", 1000}, // 27
+		{0, "0", 5},
+		{5, "5", 5},
+		{10, "10", 5},
+		{12, "10", 5},
+		{16, "15", 5},
+		{19, "20", 5},
+		{20, "20", 5},
+		{22, "20", 5},
+		{26, "25", 5},
+		{93, "95", 5},
+		{-0, "0", 5},
+		{-5, "-5", 5},
+		{-10, "-10", 5},
+		{-12, "-10", 5},
+		{-16, "-15", 5},
+		{-19, "-20", 5},
+		{-20, "-20", 5},
+		{-22, "-20", 5},
+		{-26, "-25", 5},
+	}
+	for i, datum := range data {
+		a := RoundToNearest(datum.F, datum.N)
+		s := fmt.Sprintf("%.0f", a)
+		if s != datum.E {
+			t.Errorf("expected %q got %q @ %d", datum.E, s, i)
+		}
+	}
+}
